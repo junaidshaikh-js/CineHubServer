0ab51f33e459af139968fa204e446b58c06ad3d6
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-06-30 20:43:10 +0530

    api service

---
+++ public/services/API.js
@@ -0,0 +1,28 @@
+export const API = {
+  BASE_URL: '/api',
+  fetch: async (url, args) => {
+    try {
+      const query = args ? `?${new URLSearchParams(args).toString()}` : ''
+      const res = await fetch(`${API.BASE_URL}${url}${query}`)
+      const data = await res.json()
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  getTopMovies: async () => {
+    return await API.fetch('/movies/top')
+  },
+  getRandomMovies: async () => {
+    return await API.fetch('/movies/random')
+  },
+  getMovieById: async id => {
+    return await API.fetch(`/movies/${id}`)
+  },
+  searchMovies: async (q, order, genre) => {
+    return await API.fetch(`/movies/search`, { q, order, genre })
+  },
+  getGenres: async () => {
+    return await API.fetch('/genres')
+  },
+}
