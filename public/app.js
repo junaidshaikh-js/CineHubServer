a095bf23b2cc9fde695bca911b1f1ac847118796
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-07-03 20:50:30 +0530

    feat: movie details page

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
 import { HomePage } from './components/HomePage.js'
+import { MovieDetailsPage } from './components/MovieDetailsPage.js'
 
 window.addEventListener('DOMContentLoaded', event => {
   document.querySelector('main').appendChild(new HomePage())
+
+  document.querySelector('main').appendChild(new MovieDetailsPage())
 })
 
 window.app = {
--- public/components/AnimatedLoading.js
+++ public/components/AnimatedLoading.js
@@ -4,9 +4,9 @@ export class AnimatedLoading extends HTMLElement {
   }
 
   connectedCallback() {
-    const elements = this.dataset.elements
-    const width = this.dataset.width
-    const height = this.dataset.height
+    const elements = this.dataset.elements ?? 1
+    const width = this.dataset.width ?? 100
+    const height = this.dataset.height ?? 10
 
     for (let i = 0; i < elements; i++) {
       const wrapper = document.createElement('div')
