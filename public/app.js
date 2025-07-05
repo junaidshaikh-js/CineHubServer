e4355deac4ae042367a41329d5aebbb2158a9566
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-07-05 18:54:42 +0530

    feat: error modal

--- public/app.js
+++ public/app.js
@@ -2,11 +2,19 @@ import { Router } from './services/Router.js'
 import './components/YoutubeEmbed.js'
 
 window.app = {
+  Router,
+  showError: (message = 'Something went wrong!', goToHome = true) => {
+    document.getElementById('alert-modal').showModal()
+    document.querySelector('#alert-modal p').textContent = message
+    if (goToHome) app.Router.go('/')
+  },
+  closeError: () => {
+    document.getElementById('alert-modal').close()
+  },
   search: event => {
     event.preventDefault()
     const q = document.querySelector('input[name="search"]').value
   },
-  Router,
 }
 
 window.addEventListener('DOMContentLoaded', event => {
